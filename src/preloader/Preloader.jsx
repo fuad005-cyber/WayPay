0ee5ac2e6761df4fa0d26af9efa0bf3868c2a5a7
fuad005-cyber <fuadbores@gmail.com>
2025-09-22 18:15:54 +0400

    updated

--- src/preloader/Preloader.jsx
+++ src/preloader/Preloader.jsx
@@ -1,10 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./preloader.css"
+import "./preloader.css";
 import React from "react";
 
 function Preloader() {
   return (
-    <div className="preloader-container">
+    <div className="preloader-container d-flex flex-column justify-content-center align-items-center">
       <div className="loader">
         <div className="train"></div>
         <div className="track">
@@ -12,9 +12,8 @@ function Preloader() {
           <span></span>
           <span></span>
         </div>
-        
       </div>
-      <h1>WayPay</h1>
+      <h1 className="mt-4 text-primary fw-bold display-5 display-md-4 display-lg-3 text-center">WayPay</h1>
     </div>
   );
 }
